f5f214046aa2eb44191ca6a54369f626249fe8f6
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-28 18:14:19 -0500

    built out character class

---
+++ character.py
@@ -0,0 +1,19 @@
+class Character:
+    def __init__ (self, name, coin_level):
+        self.name = name
+        self.coin_level = coin_level
+
+    def move(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def get_coins(self):
+        """
+        method takes in instance of coin class to generate random amounts of coins
+        """
+        pass
+
+    def show_coins(self):
+        return self.coin_level
