68da9e2af61a68215d757c8552118bb6834b9125
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 14:23:23 -0500

    built out add_coin and sub_coin methods

--- coin.py
+++ coin.py
@@ -1,9 +1,13 @@
+import random
+
 class Coins():
     def __init__(self):
         pass
 
-    def add_coin(self, amount):
-        pass
+    def add_coin(self):
+        add_amount = random.randint(1,50)
+        return add_amount
 
-    def subtract_coin(self, amount):
-        pass
+    def subtract_coin(self):
+        sub_amount = random.randint(1,50)
+        return sub_amount
